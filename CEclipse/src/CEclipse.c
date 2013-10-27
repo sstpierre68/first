f0bfeed81362d3c401a0c8b2b81cd73fbb815b84
Stephane St-Pierre <sstpierre68@gmail.com>
2013-10-27 14:24:11 -0400

    First git commit

---
+++ CEclipse/src/CEclipse.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	typedef struct peewee
+	{
+		char *prenom;
+		char *nom;
+		unsigned short age;
+		int no;
+	} ble;
+
+	struct myStruct
+	{
+		char *prenom;
+		char *nom;
+		unsigned short age;
+	} gtg;
+
+	struct myStruct so;
+	//so.no = 7;
+	so.age = 10;
+	so.prenom = "Bobe";
+	so.nom = "Popolich";
+
+	ble mon_ble;
+
+	gtg.age = 93;
+	mon_ble.nom = "jimi";
+
+	//system("COLOR 6C");
+	//system("ls -l");
+	printf("\033[22;31mHello, world!");
+
+	//int i;
+	for(int i = 0;i < argc; i++)
+	{
+		printf("nbr argc: %d\n", i);
+		printf("nbr argv[]: %s\n", argv[i]);
+	}
+
+	printf("\nDecimal: %d\nFloat: %15.15f\nOctale: %o\nHexadecimal: %010d\n", 57, 57.0, 57, 'A');
+
+	printf("%s %s, %d, %d, %s", so.prenom, so.nom, so.age, gtg.age, mon_ble.nom);
+	printf("\033[22;31mHello, world!");
+
+	return 0;
+}
+
+
+
+
+
